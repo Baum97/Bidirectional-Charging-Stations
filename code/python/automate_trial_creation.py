d37e8dbb1848f11b86e47073c1bbb6a26954f996
BackendChestDev <47142967+Baum97@users.noreply.github.com>
2025-07-20 01:54:42 +0200

    advance dev of automation #26 #27 #28
    added necessary code to implement automation of creation for the files necessary in a sumocfg simulation

--- code/python/automate_trial_creation.py
+++ code/python/automate_trial_creation.py
@@ -1,8 +1,75 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 import sys
+import subprocess
+import os
+
+
+#! custom input ####################################################!
+
+
+java_files = ["Main.java", "MatsimPlans2SumoTrips.java", "NearestLinkResult.java", "NearestLinkUtil.java", 
+                "NetworkBuilderUtil.java", "OSM2Network.java", "POICentroid.java"]
+
+    # input_args= 1: file.osm, 2: csv file(s)
+java_args = ["reutlingen.osm", "POIdata.csv"]
+
+    # matsim= MATsim.net.xml, netxml= output file name (recommended: [city_name].net.xml)
+netc_matsim = "MATsReutlingenNet.xml"
+netc_netxml = "Reutlingen.net.xml"
+
+#! custom input ####################################################!
+
+
+
+
+# custom functions ################################################
+
+def compile_java_file(input_files):
+    compile_process = subprocess.run(
+        ["javac"] + input_files,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
+    if (compile_process.returncode != 0):
+        print("Compile Process failed:")
+        print(compile_process.stderr)
+        exit(1)
+    else:
+        print("Compilation successful")
+
+
+    # input_args= 1: file.osm, 2: csv file(s)
+def execute_java_file(input_args):
+    subprocess.run(["java", "Main"] + input_args)
+
+    # input_args= 1: MATsim.net.xml 2: output file name (recommended: [city_name].net.xml)
+def execute_netconvert(input_args):
+    command = ["netconvert", input_args]
+    subprocess.run(command)
+
+
+    # input_args=
+def execute_matsim_importPlans(matsim_pop, route_xml):
+    command = ["python", ".\tools\import\matsim\matsim_importPlans.py", 
+               "--plan-file", matsim_pop, "-o", route_xml]
+
+# custom functions ################################################
+
+
+
+
 
 def main(input_file, output_file):
+
+
+
+
+
+
+
     residential_buildings = {
         "apartments", "house", "residential", "detached",
         "semidetached_house", "terrace", "dormitory"
@@ -153,6 +220,13 @@ def main(input_file, output_file):
     df = pd.DataFrame(results)
     df.to_csv(output_file, index=False, encoding="utf-8")
     print(f"{len(df)} Wohngebäude gespeichert in '{output_file}'")
+
+
+    # java files dir- D:\Master\Forschungsprojekt\GitDir\Bidirectional-Charging-Stations\matsim\matsim-project\src\main\java\org\matsim\project\v1
+    # compile all necessary java files to then call main.java to create MATsim files
+    compile_java_file(java_files)
+    execute_java_file(java_args)
+    execute_netconvert(netc_args)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
