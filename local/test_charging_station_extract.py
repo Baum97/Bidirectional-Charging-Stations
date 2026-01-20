38e9c35498dc6a98632c5f7c80ebc66bdcf20baa
BackendChestDev <47142967+Baum97@users.noreply.github.com>
2026-01-20 22:45:25 +0100

    cleanup

--- local/test_charging_station_extract.py
+++
@@ -1,3 +0,0 @@
-from biflex_local_runner import extract_real_charging_stations
-
-real_charging_stations = extract_real_charging_stations(r"C:\Users\erikw\Desktop\AIM\bilfex-container\data\scenarios\test11\osm.net.xml.gz")
